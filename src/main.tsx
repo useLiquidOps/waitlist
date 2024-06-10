af513b337d867240a92242fdf1bb4c569da616a4
Lorimer Jenkins <lorimerjenkins1@gmail.com>
2024-06-10 20:09:38 +0100

    fix: wallet kit and w connect

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-// import BrowserWalletStrategy from "@arweave-wallet-kit-beta/browser-wallet-strategy";
-// import ArConnectStrategy from "@arweave-wallet-kit-beta/arconnect-strategy";
-// import OthentStrategy from "@arweave-wallet-kit-beta/othent-strategy";
-// import { ArweaveWalletKit } from "@arweave-wallet-kit-beta/react";
+import BrowserWalletStrategy from "@arweave-wallet-kit-beta/browser-wallet-strategy";
+import ArConnectStrategy from "@arweave-wallet-kit-beta/arconnect-strategy";
+import OthentStrategy from "@arweave-wallet-kit-beta/othent-strategy";
+import { ArweaveWalletKit } from "@arweave-wallet-kit-beta/react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import React from "react";
@@ -10,7 +10,7 @@ import { WalletConnectWrapper } from "./utils/wallets/walletConnect.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WalletConnectWrapper>
-    {/* <ArweaveWalletKit
+    <ArweaveWalletKit
       config={{
         strategies: [new ArConnectStrategy(), new OthentStrategy(), new BrowserWalletStrategy()],
         permissions: [
@@ -21,12 +21,12 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         ensurePermissions: true
       }}
       theme={{ displayTheme: "light" }}
-    > */}
+    >
       
       <App />
 
       
-    {/* </ArweaveWalletKit> */}
+    </ArweaveWalletKit>
 
     </WalletConnectWrapper>
     
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -170,7 +170,7 @@ export default function Home() {
         if (mode === "eth" && !signMsgData) return;
 
         const res = await (
-          await fetch(`https://waitlist-server.lorimer.pro/record-address`, {
+          await fetch(`http://localhost:3001/record-address`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
@@ -214,7 +214,7 @@ export default function Home() {
           <SectionTitle>Join the waitlist!</SectionTitle>
           <Spacer y={0.6} />
           <Paragraph>
-            Connect your wallet to be added to our wait list leader board and be notified when we launch.
+            Connect your wallet to be added to our waitlist leader board and be notified when we launch.
             
             Don't worry, we won't spam you!
           </Paragraph>
@@ -231,10 +231,10 @@ export default function Home() {
                 {(!connected && (
                   <>
                     <Button onClick={() => connect()}>
-                      Arweave
+                      Arweave wallet
                     </Button>
                     <Button onClick={() => open()}>
-                      Ethereum
+                      Ethereum wallet
                     </Button>
                   </>
                 )) || (
@@ -290,68 +290,76 @@ export default function Home() {
         </Form>
         <Spacer y={0.01} />
         <Leaderboard>
-          <Stats>
-            <Stat>
-              <h4>{stats.users.toLocaleString()}</h4>
-              <Paragraph>Wait list sign ups</Paragraph>
-            </Stat>
-            <Stat>
-              <h4>
-                {(stats.arTokens * arPrice).toLocaleString(undefined, {
-                  style: "currency",
-                  currency: "USD",
-                  currencyDisplay: "narrowSymbol",
-                  maximumFractionDigits: 2,
-                }) + " USD"}
-              </h4>
-              <Paragraph>User-controlled assets</Paragraph>
-            </Stat>
-          </Stats>
-          <Spacer y={1} />
-          <Table>
-            <thead>
-              <tr>
-                <th></th>
-                <th>Address</th>
-                <th>USD Balance</th>
-                <th>AR Balance</th>
-              </tr>
-            </thead>
-            <AnimatePresence>
-              <tbody>
-                {users.map((p, i) => (
-                  <motion.tr
-                    initial={{ opacity: 0, scale: 0.93 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    exit={{ opacity: 0, scale: 0.93 }}
-                    key={i}
-                  >
-                    <td>{i + 1}.</td>
-                    <td>{p.address}</td>
-                    <td>
-                      {(p.balance * arPrice).toLocaleString(undefined, {
-                        style: "currency",
-                        currency: "USD",
-                        currencyDisplay: "narrowSymbol",
-                        maximumFractionDigits: 2,
-                      }) + " USD"}
-                    </td>
-                    <td>
-                      {p.balance.toLocaleString(undefined, {
-                        maximumFractionDigits: 2,
-                      })}{" "}
-                      AR
-                    </td>
-                  </motion.tr>
-                ))}
-              </tbody>
-            </AnimatePresence>
-          </Table>
-          {users.length === 0 && (
-            <Paragraph></Paragraph>
-          )}
-          <Spacer y={1} />
-        </Leaderboard>
+  <Stats>
+    <Stat>
+      <h4>{stats.users.toLocaleString()}</h4>
+      <Paragraph>Wait list sign ups</Paragraph>
+    </Stat>
+    <Stat>
+      <h4>
+        {(stats.arTokens * arPrice).toLocaleString(undefined, {
+          style: "currency",
+          currency: "USD",
+          currencyDisplay: "narrowSymbol",
+          maximumFractionDigits: 2,
+        }) + " USD"}
+      </h4>
+      <Paragraph>User-controlled assets</Paragraph>
+    </Stat>
+  </Stats>
+  <Spacer y={1} />
+  <Table>
+    <thead>
+      <tr>
+        <th></th>
+        <th>Address</th>
+        <th>USD Balance</th>
+        <th>Token Balance</th>
+      </tr>
+    </thead>
+    <AnimatePresence>
+      <tbody>
+        {users.map((p, i) => (
+          <motion.tr
+            initial={{ opacity: 0, scale: 0.93 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.93 }}
+            key={i}
+          >
+            <td>{i + 1}.</td>
+            <td>{p.address}</td>
+            <td>
+              {typeof p.balance === "number"
+                ? (p.balance * arPrice).toLocaleString(undefined, {
+                    style: "currency",
+                    currency: "USD",
+                    currencyDisplay: "narrowSymbol",
+                    maximumFractionDigits: 2,
+                  }) + " USD"
+                : "N/A"}
+            </td>
+            <td>
+              {typeof p.balance === "number"
+                ? p.balance.toLocaleString(undefined, {
+                    maximumFractionDigits: 2,
+                  }) + " AR"
+                : Object.entries(p.balance)
+                    .map(
+                      ([token, balance]) =>
+                        `${balance.toLocaleString(undefined, {
+                          maximumFractionDigits: token === "eth" ? 4 : 2,
+                        })} ${token.toUpperCase()}`
+                    )
+                    .join(", ")}
+            </td>
+          </motion.tr>
+        ))}
+      </tbody>
+    </AnimatePresence>
+  </Table>
+  {users.length === 0 && <Paragraph></Paragraph>}
+  <Spacer y={1} />
+</Leaderboard>
       </Wrapper>
     </>
   );
